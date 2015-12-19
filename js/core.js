8b122a3f296247ca4f778b2d345115fa17cb095e
Chip Bell <chip.bell@spinen.com>
2015-12-18 23:52:59 -0500

    Stubbing arrow

--- js/core.js
+++ js/core.js
@@ -2,6 +2,13 @@ L.Control.CheckboxTree = L.Control.extend({
   onAdd: function(map) {
     var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar checkbox-tree topright');
     container.innerText = 'Hello world';
+
+    L.Control.CheckboxTree.stubParentRow({
+      text: 'FAA',
+      id: 'FAA',
+      container: container
+    }); 
+
     return container;
   }
 });
--- js/parent-row.js
+++ js/parent-row.js
@@ -1,8 +1,31 @@
 (function() {
+
+  var toggleArrow = function(evt) {
+    var parentRow = evt.target.parentNode;
+
+    // remove the expanded class if its there. Otherwise, add it
+    if(parentRow.className.indexOf('expanded') == -1) {
+      parentRow.className += ' expanded';
+      return;
+    }
+
+    parentRow.className = 'parent-row';
+  };
+
   L.Control.CheckboxTree.stubParentRow = function(options) {
     // defaults
+    options.id = options.id || '';
     options.onClick = options.onClick || function() {};
     options.text = options.text || '';
     options.children = options.children || [];
+    options.container = options.container || null;
+
+    var row = L.DomUtil.create('div', 'parent-row', options.container);
+    row.id = options.id;
+
+    var arrow = L.DomUtil.create('span', 'arrow', row);
+    arrow.addEventListener('click', toggleArrow);
+
+    return row;
   };
 })();
