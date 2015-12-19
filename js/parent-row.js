3f3c2bdaff574438061b0b291329d76260febd08
Chip Bell <chip.bell@spinen.com>
2015-12-18 23:38:22 -0500

    Stubbing defaults

--- js/parent-row.js
+++ js/parent-row.js
@@ -1,4 +1,9 @@
 (function() {
   L.Control.CheckboxTree.stubParentRow = function(options) {
+    // defaults
+    options.text = options.text || '';
+    options.onClick = options.onClick || function() {};
+    options.text = options.text || '';
+    options.children = options.children || [];
   };
 })();
