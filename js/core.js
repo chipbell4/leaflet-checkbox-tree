38b857bcd7b8cc41d5c4990a714d08d5a09c4a07
Chip Bell <chip.bell@spinen.com>
2015-12-18 23:07:21 -0500

    Stubbing out new code

--- js/core.js
+++ js/core.js
@@ -1,1 +1,7 @@
-var x = 0;
+L.Control.CheckboxTree = L.Control.extend({
+  onAdd: function(map) {
+    var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar checkbox-tree topright');
+    container.innerText = 'Hello world';
+    return container;
+  }
+});
