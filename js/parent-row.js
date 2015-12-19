c0dade20c5bad2bc284e76ebb932ec409f3b00cd
Chip Bell <chip.bell@spinen.com>
2015-12-18 23:40:28 -0500

    Extra option?

--- js/parent-row.js
+++ js/parent-row.js
@@ -1,7 +1,6 @@
 (function() {
   L.Control.CheckboxTree.stubParentRow = function(options) {
     // defaults
-    options.text = options.text || '';
     options.onClick = options.onClick || function() {};
     options.text = options.text || '';
     options.children = options.children || [];
