86e67ab4a3e96978b49d9be35c226e14d9c1afd3
Chip Bell <chip.bell@spinen.com>
2015-12-18 23:56:46 -0500

    Adding some comments

--- js/parent-row.js
+++ js/parent-row.js
@@ -27,13 +27,18 @@
     var row = L.DomUtil.create('div', 'parent-row', options.container);
     row.id = options.id;
 
+    // Add the arrow
     var arrow = L.DomUtil.create('span', 'arrow', row);
     arrow.addEventListener('click', toggleArrow);
 
     var label = L.DomUtil.create('label', '', row);
+
+    // Add the checkbox
     var checkbox = L.DomUtil.create('input', '', label);
     checkbox.type = 'checkbox';
     checkbox.addEventListener('click', onChecked);
+
+    // Add the text label
     var text = L.DomUtil.create('span', '', label);
     text.innerText = options.text;
 
