f70a703fa78a0296758bdc2b0defbb14410f06e4
Chip Bell <chip.bell@spinen.com>
2015-12-19 10:39:47 -0500

    Allow parents with no children to be emitted during an event as well

--- js/determine-checked-state.js
+++ js/determine-checked-state.js
@@ -1,13 +1,24 @@
 (function() {
   L.Control.CheckboxTree.determineCheckedState = function(container) {
+    // Get all checked and unchecked *child* items
     var li = container.getElementsByTagName('li');
-    
-    // Separate items by being checked or unchecked
     var uncheckedItems = [];
     var checkedItems = [];
     var N = li.length;
     for(var k = 0; k < N; k++) {
       var checkbox = li[k].firstChild.firstChild;
+      if(checkbox.checked) {
+        checkedItems.push(checkbox.nextSibling.textContent);
+      } else {
+        uncheckedItems.push(checkbox.nextSibling.textContent);
+      }
+    }
+
+    // get all parents with no children that are checked
+    var rows = container.getElementsByClassName('no-children');
+    N = rows.length;
+    for(var k = 0; k < N; k++) {
+      var checkbox = rows[k].getElementsByTagName('input')[0];
       if(checkbox.checked) {
         checkedItems.push(checkbox.nextSibling.textContent);
       } else {
